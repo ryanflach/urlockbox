42fd573cea58076a6b16947b5c2af4741dda544f
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-31 16:02:11 -0600

    [WIP] Progress on read status update

--- app/assets/javascripts/link.js
+++ app/assets/javascripts/link.js
@@ -5,26 +5,40 @@ $(document).ready(() => {
 const handleReadStatusUpdate = () => {
   $('#links .status').on('click', (e) => {
     e.preventDefault();
-    const id = $(e.target).closest('tr')[0].id
-    const
-    console.log(id);
+    const id = $(e.target).closest('tr')[0].id;
     $.ajax({
       url: '/links/' + id,
       method: 'put',
       data: { id: id }
-    }).then(updateLink)
+    }).then(createLinkHTML)
+    .then(reRenderLink)
     .fail(handleError);
   });
 };
 
-const updateLink = (linkData) => {
-  let counter = 0;
-  $('#' + linkData.id)[0].children.forEach((child) => {
-    if (counter === 0) { child.toggleClass('.read-true') }
-    counter++;
-  })
+const createLinkHTML = (linkData) => {
+  return({
+    id: linkData.id,
+    html: "<tr id='" + linkData.id + "'>" +
+    "<td class='read-" + linkData.read + "'>" + linkData.title + "</td>" +
+    "<td class='read-" + linkData.read + "'><a href='" + linkData.url + "'>" + linkData.url + "</a></td>" +
+    "<td class='status'><a href='#'>" + newReadStatusText(linkData.read) + "</a></td>" +
+    "<td><a href='/links/" + linkData.id + "/edit' class='btn btn-xs btn-default'> Edit </a></td>"
+  });
+};
+
+const reRenderLink = (link) => {
+  $('#' + link.id).replaceWith(link.html);
+};
+
+const newReadStatusText = (current) => {
+  if (current === 'true') {
+    return 'Mark as Unread';
+  } else {
+    return 'Mark as Read';
+  }
 };
 
 const handleError = (error) => {
-  console.log(error)
+  console.log(error);
 };
