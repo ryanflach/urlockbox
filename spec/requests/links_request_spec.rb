58561fbac81ef57ae9782033540a6f9d62afa75e
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-08 12:34:34 -0700

    Add controller test for destroy action

--- spec/requests/links_request_spec.rb
+++ spec/requests/links_request_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe Api::V1::LinksController, type: :request do
     expect(returned_json['title']).to eq(link.title)
     expect(returned_json['url']).to eq(link.url)
   end
+
+  it 'returns JSON data when deleting a link' do
+    user = User.create!(email: 'hello@example.com', password: 'pass')
+
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).
+      and_return(user)
+
+    link = user.links.create!(
+      title: 'Google',
+      url: 'http://www.google.com'
+    )
+
+    delete "/api/v1/links/#{link.id}"
+
+    returned_json = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+    expect(response.content_type).to eq('application/json')
+    expect(returned_json['title']).to eq(link.title)
+    expect(returned_json['url']).to eq(link.url)
+    expect(Link.count).to eq(0)
+  end
 end
