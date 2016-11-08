c529f1ef96077e8d675ace26121bbcd940632a50
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-08 10:05:18 -0700

    Add ability to add tags upon link creation

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,13 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:edit, :update]
-  
+
   def index
     redirect_to login_path and return if !current_user
     @links = Link.where(user: current_user)
   end
 
   def create
-    @link = Link.new(link_params)
+    @link = Link.new(parsed_link_params)
     if @link.save
       flash[:success] = "Link to #{@link.title} successfully added."
       redirect_to links_path
@@ -21,7 +21,7 @@ class LinksController < ApplicationController
   end
 
   def update
-    if @link.update(link_params)
+    if @link.update(parsed_link_params)
       flash[:success] = "Link updated successfully."
       redirect_to links_path
     else
@@ -33,10 +33,22 @@ class LinksController < ApplicationController
   private
 
   def link_params
-    params.require(:link).permit(:url, :title, :user_id)
+    params.require(:link).permit(:url, :title, :user_id, :tags)
   end
 
   def set_link
     @link = Link.find(params[:id])
   end
+
+  def parsed_link_params
+    clean_params = link_params
+    clean_params[:tags] = stripped(clean_params[:tags])
+    clean_params
+  end
+
+  def stripped(words)
+    separated = words.downcase.split(',')
+    separated.map { |word| Tag.find_or_create_by(name: word.strip) }
+  end
+
 end
